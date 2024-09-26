ee2b68d338f2776b19833c2778c0af5b9588b66f
erlend <wildifyme@gmail.com>
2024-09-26 14:02:43 +0200

    start trying to pass prop from parent...
    ... need to figure it out

--- src/components/inlineNavigation.vue
+++ src/components/inlineNavigation.vue
@@ -1,12 +1,19 @@
-<script lang="ts">
+<script setup lang="ts">
 
-
+const props = defineProps<{
+    pages?: any[]
+}>();
 
 </script>
 
 <template>
     <div style="display: flex; flex-direction: column; font-style:italic;">
+        <template v-for="page in pages">
+            <a href={{ page.href }}>{{ page.title }}</a>
+        </template>
+        <!-- 
         <a href="/">Home, sweet home</a>
         <a href="/about/">About</a>
+         -->
     </div>
 </template>
