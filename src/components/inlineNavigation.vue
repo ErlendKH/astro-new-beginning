bb446cecbce32a276cb455e0950726cbf17cb3a2
erlend <wildifyme@gmail.com>
2024-09-26 13:46:40 +0200

    inline navigation

---
+++ src/components/inlineNavigation.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+
+
+</script>
+
+<template>
+    <div style="display: flex; flex-direction: column; font-style:italic;">
+        <a href="/">Home, sweet home</a>
+        <a href="/about/">About</a>
+    </div>
+</template>
